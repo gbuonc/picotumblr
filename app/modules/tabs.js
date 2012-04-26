e091f154791a5992bfa21da0630e9052a1b5a9b6
Gianluca Buoncompagni <gbuonc@gmail.com>
2012-04-26 11:37:12 +0200

    plash screen and touch icon iOs

--- app/modules/tabs.js
+++ app/modules/tabs.js
@@ -5,17 +5,74 @@ define([ '../../assets/js/text!templates/hlist.tpl',
          'modules/app'], 
    function(listTpl, handlebars, iScrl, lawnch, app){  
    var tabs ={
+      hpTabs:{
+         history:{
+            $hpList : $('#history .scrollWrapper'),
+            $clearBtn : $('#history button'),
+            $resetBtn : $('#recentTab .btnClear'),
+            init: function(){
+               hpTabs.history.$clearBtn.live('click', function(){
+                  var item = $(this).attr('data-target');
+                  tabs.clearItem(item);
+               });
+               hpTabs.history.$resetBtn.live('click', function(){            
+                  tabs.reset();        
+               });
+            },
+            scrollable : new iScroll('history')
+         },
+         favourites:{
+            $hpList : $('#history .scrollWrapper'),
+            $clearBtn : $('#history button'),
+            $resetBtn : $('#recentTab .btnClear'),
+            init: function(){
+               hpTabs.history.$clearBtn.live('click', function(){
+                  var item = $(this).attr('data-target');
+                  tabs.clearItem(item);
+               });
+               hpTabs.history.$resetBtn.live('click', function(){            
+                  tabs.reset();        
+               });
+            },
+            scrollable : new iScroll('history')
+         },
+         related:{
+            $hpList : $('#history .scrollWrapper'),
+            $clearBtn : $('#history button'),
+            $resetBtn : $('#recentTab .btnClear'),
+            init: function(){
+               hpTabs.history.$clearBtn.live('click', function(){
+                  var item = $(this).attr('data-target');
+                  tabs.clearItem(item);
+               });
+               hpTabs.history.$resetBtn.live('click', function(){            
+                  tabs.reset();        
+               });
+            },
+            scrollable : new iScroll('history')
+         }   
+      },
       source: $(listTpl).html(),
       template: function(){
          Handlebars.compile(source);
          console.log(this);
          return this;
+      },
+      add: function(){
+         
+      },
+      remove: function(){
+         
+      },      
+      updateHpList: function(){
+         
+      },
+      reset: function(){
+         
+      },
+      store: function(){
+         
       }
-      // init: function(){
-            //          var source = $(listTpl).html();
-            //          var template = Handlebars.compile(source); 
-            //          return template;
-            //       }         
    }
    return tabs;   
 });
